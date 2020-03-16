f4bf657efb2b6b107d513becfdeee0a32682c573
Kirill Mager <hi.kirill.m@gmail.com>
2020-03-16 14:28:06 +0300

    fix: add logic to state change using the useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import TodoList from './TodoList'
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
-  }
+export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, title: 'First todo', completed: false},
+    {id: 2, title: 'Second todo', completed: true},
+  ]);
+  const [todoTitle, setTodoTitle] = useState('');
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Todo app</h1>
+  const addTodo = event => {
+    if (event.key === 'Enter') {
+      setTodos([...todos, {id: Date.now(), title: todoTitle, completed: false}])
+      setTodoTitle('')
+    }
+  };
+  
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
+        <div className="input-field">
+          <input 
+            type="text"
+            value={todoTitle}
+            onChange={event => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+            />
+          <label>Todo name</label>
+        </div>
 
-          <TodoList todos={this.state.todos} />
-      </div>
-    );
-  }
+        <TodoList todos={todos} />
+    </div>
+  );
 }
